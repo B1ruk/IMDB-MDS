61e3e321aaa2b7768059f2c9d33bbf6d0cba6a61
b1ruk <biruk3870@gmail.com>
2026-02-26 06:46:24 +0300

    create factory for movieProcessor

--- src/main/java/io/java/essentials/Main.java
+++ src/main/java/io/java/essentials/Main.java
@@ -3,7 +3,6 @@ package io.java.essentials;
 import io.java.essentials.model.Movie;
 import io.java.essentials.processor.MovieProcessor;
 import io.java.essentials.processor.fileProcessor.CsvMovieProcessor;
-import io.java.essentials.processor.fileProcessor.FileMovieProcessor;
 import io.java.essentials.processor.fileProcessor.JsonMovieProcessor;
 import io.java.essentials.processor.mapper.CsvMovieMapper;
 import io.java.essentials.processor.mapper.JsonMovieMapper;
@@ -12,19 +11,30 @@ import io.java.essentials.processor.mapper.MovieMapper;
 public class Main {
     public static void main(String[] args) {
 
-        MovieMapper csvMovieMapper = new CsvMovieMapper();
-        MovieMapper jsonMovieMapper = new JsonMovieMapper();
+        MovieProcessor movieProcessor = movieProcessorFactory( "imdb_top_1000.csv");
+        MovieProcessor movieJsonProcessor = movieProcessorFactory("imdb_top_1000_json_file.json");
+//        MovieProcessor movieXlProcessor = movieProcessorFactory("imdb_top_1000_json_file.xls");
 
-        MovieProcessor movieProcessor=new CsvMovieProcessor(csvMovieMapper,"imdb_top_1000.csv");
-        MovieProcessor movieJsonProcessor=new JsonMovieProcessor(jsonMovieMapper,"imdb_top_1000_json_file.json");
+        printResult(movieProcessor);
+        printResult(movieJsonProcessor);
+    }
 
+    public static MovieProcessor movieProcessorFactory(String fileName) {
+        String extension = fileName.split("\\.")[1];
 
-        for (Movie movie : movieProcessor.loadMovies()) {
-            System.out.println(movie.seriesTitle()+"...."+movie.releasedYear());
+        switch (extension) {
+            case "csv":
+                return new CsvMovieProcessor(new CsvMovieMapper(), fileName);
+            case "json":
+                return new JsonMovieProcessor(new JsonMovieMapper(), fileName);
         }
+        throw new RuntimeException("unable to resolve extension");
 
-        for (Movie movie : movieJsonProcessor.loadMovies()) {
-            System.out.println(movie.seriesTitle()+"*******"+movie.releasedYear());
+    }
+
+    private static void printResult(MovieProcessor movieProcessor) {
+        for (Movie movie : movieProcessor.loadMovies()) {
+            System.out.println(movie.seriesTitle() + "...." + movie.releasedYear());
         }
     }
 }
