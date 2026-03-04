0341aee36f51c899ed2befc17fe7be3f18404620
b1ruk <biruk3870@gmail.com>
2026-03-04 05:53:15 +0300

    executor service and CompletableFuture

--- src/main/java/io/java/essentials/storage/MovieStorage.java
+++ src/main/java/io/java/essentials/storage/MovieStorage.java
@@ -3,5 +3,5 @@ package io.java.essentials.storage;
 import io.java.essentials.processor.MovieProcessor;
 
 public interface MovieStorage {
-    void pollMovieRecords(MovieProcessor rocessor);
+    void pollMovieRecords(MovieProcessor processor);
 }
--- src/main/java/io/java/essentials/storage/ScheduledMovieStorage.java
+++ src/main/java/io/java/essentials/storage/ScheduledMovieStorage.java
@@ -1,33 +1,64 @@
 package io.java.essentials.storage;
 
-import io.java.essentials.model.Movie;
 import io.java.essentials.processor.MovieProcessor;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.concurrent.*;
-import java.util.function.Supplier;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+//main
 
 public class ScheduledMovieStorage implements MovieStorage {
     private static Logger LOGGER = Logger.getLogger(ScheduledMovieStorage.class.getSimpleName());
 
-    private ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+    private ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(4);
 
     @Override
     public void pollMovieRecords(MovieProcessor processor) {
-        CompletableFuture.supplyAsync(processor::loadMovies)
-                .thenAccept(movies -> LOGGER.info("Loaded " + movies.size() + " movies"))
+        var thread = new Thread(() -> {
+            processor.loadMovies();
+            System.out.println("Loaded movies at " + Thread.currentThread().getName());
+        });
+
+        thread.start();
+        thread.run();
+
+        //using executor service
+        var executor = Executors.newFixedThreadPool(5);
+        executor.submit(() -> {
+            processor.loadMovies();
+            System.out.println("Loaded movies using executor service " + Thread.currentThread().getName());
+        });
+
+
+//Chaining CompletableFuture to load and process movie records asynchronously
+        CompletableFuture.supplyAsync(() -> processor.loadMovies())
+                .thenApply(movieRecord -> {
+                    LOGGER.info("Processing movie records using compltableFuture" + Thread.currentThread().getName());
+                    return movieRecord.stream()
+                            .limit(10)
+                            .toList();
+                })
+                .thenAccept(movies -> {
+                    LOGGER.info("CompletableFuture thenAccept" + Thread.currentThread().getName());
+                    LOGGER.info("Loaded " + movies.size() + " movies");
+                })
                 .exceptionally(ex -> {
                     LOGGER.log(Level.SEVERE, "Failed to load movies", ex);
                     return null;
                 })
-                        .join();
+                .join();
 
+
+        //Using scheduled executor service to poll movie records every 5 seconds
         scheduledExecutorService.scheduleAtFixedRate(() -> {
             LOGGER.log(Level.INFO, "processing movie records {0}", LocalDateTime.now());
+            LOGGER.log(Level.INFO, "processing movie records {0}", Thread.currentThread().getName());
             processor.loadMovies();
-        }, 5, 3, TimeUnit.SECONDS);
+        }, 0, 5, TimeUnit.SECONDS);
     }
 }
